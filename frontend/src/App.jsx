bd9d5dcc392f3ed2dbfb00a4409e72bbfd1ca3e5
Chaitali <gawdechaitali585@gmail.com>
2026-09-20 09:29:28 +0530

    Restore original App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React,{useEffect} from 'react';
-import {Navigate,Route,Routes} from 'react-router-dom';
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Navbar';
 import Sidebar from './Pages/Sidebar';
 import Home from './Pages/Home';
@@ -8,30 +8,50 @@ import CertificatePage from './Pages/CertificatePage ';
 import LoginPage from './Pages/Auth/LoginPage';
 import SignupPage from './Pages/Auth/SignupPage';
 import useAuthStore from './stores/useAuthStore';
-import {Toaster} from 'react-hot-toast';
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
 import MySubmissionsPage from './Pages/MySubmissionsPage';
 import ProjectDetailPage from './Pages/ProjectDetailPage';
 
-const App=()=>{
- const {authUser,checkAuth}=useAuthStore();
- useEffect(()=>{checkAuth();},[]);
- return <div className="flex flex-col h-screen">
-  <Toaster/>
-  {authUser&&<Navbar/>}
-  <div className="flex flex-1 overflow-hidden">
-   {authUser&&<Sidebar/>}
-   <main className="flex-1 overflow-auto">
-    <Routes>
-     <Route path="/" element={authUser?<Home/>:<Navigate to="/signup"/>}/>
-     <Route path="/new-submission" element={authUser?<SubmitPage/>:<Navigate to="/signup"/>}/>
-     <Route path="/my-submissions" element={authUser?<MySubmissionsPage/>:<Navigate to="/signup"/>}/>
-     <Route path="/projects/:projectId" element={authUser?<ProjectDetailPage/>:<Navigate to="/signup"/>}/>
-     <Route path="/certificate/:projectId" element={authUser?<CertificatePage/>:<Navigate to="/signup"/>}/>
-     <Route path="/signup" element={!authUser?<SignupPage/>:<Navigate to="/"/>}/>
-     <Route path="/login" element={!authUser?<LoginPage/>:<Navigate to="/"/>}/>
-    </Routes>
-   </main>
-  </div>
- </div>
+const App = () => {
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (authUser !== null) {
+      console.log("✅ Auth user:", authUser);
+    }
+  }, [authUser]);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Toaster />
+      {/* Navbar at top */}
+      {authUser && <Navbar />}
+
+      {/* Sidebar + Page Content */}
+      <div className="flex flex-1 overflow-hidden">
+        {authUser && <Sidebar />}
+
+        <main className="flex-1 overflow-auto ">
+          <Routes>
+            <Route path="/" element={authUser ?<Home />:<Navigate to={'/signup'} />} />
+            <Route path="/new-submission" element={authUser ?<SubmitPage />:<Navigate to={'/signup'} />} />
+            <Route path="/my-submissions" element={authUser ?<MySubmissionsPage />:<Navigate to={'/signup'} />} />
+            <Route path="/projects/:projectId" element={authUser ? <ProjectDetailPage /> : <Navigate to={'/signup'} />} />
+
+            <Route path="/certificate/:projectId" element={authUser ?<CertificatePage />:<Navigate to={'/signup'} />} />
+            <Route path='/signup' element={!authUser ? <SignupPage /> : <Navigate to={'/'} />} />
+            <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={'/'} />} />
+            {/* <Route path='/test' element={<MyProjectsPage />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
 };
+
 export default App;
