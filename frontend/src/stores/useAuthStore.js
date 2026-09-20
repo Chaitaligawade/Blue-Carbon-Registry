f83f7e228b54a4c4e4ac5d6cd3f64aa997c6eba8
Chaitali <gawdechaitali585@gmail.com>
2026-09-20 08:29:56 +0530

    Upload frontend/src/stores/useAuthStore.js

---
+++ frontend/src/stores/useAuthStore.js
@@ -0,0 +1,32 @@
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
+import {axiosInstance} from "../lib/axios";
+import toast from "react-hot-toast";
+
+const useAuthStore=create(persist((set)=>({
+ authUser:null,isCheckingAuth:false,isSigningUp:false,isLoggingIn:false,
+ signup:async(data)=>{
+  set({isSigningUp:true});
+  try{const res=await axiosInstance.post("/auth/signup",data);set({authUser:res.data});toast.success("Account created successfully");}
+  catch(error){const errMsg=error?.response?.data?.error||error.message||"Signup failed";console.error("Signup Error:",errMsg);toast.error(errMsg);}
+  finally{set({isSigningUp:false});}
+ },
+ login:async(data)=>{
+  set({isLoggingIn:true});
+  try{const res=await axiosInstance.post("/auth/login",data);set({authUser:res.data});toast.success("Logged in successfully");}
+  catch(error){const errMsg=error?.response?.data?.error||error.message||"Login failed";console.error("Login Error:",errMsg);toast.error(errMsg);}
+  finally{set({isLoggingIn:false});}
+ },
+ logout:async()=>{
+  try{await axiosInstance.post("/auth/logout");set({authUser:null,isSigningUp:false,isLoggingIn:false,isCheckingAuth:false});toast.success("Logged out successfully");}
+  catch(error){const errMsg=error?.response?.data?.error||error.message||"Logout failed";console.error("Logout Error:",errMsg);toast.error(errMsg);}
+ },
+ checkAuth:async()=>{
+  set({isCheckingAuth:true});
+  try{const res=await axiosInstance.get("/auth/check");set({authUser:res.data});}
+  catch(error){console.log("Error in checkAuth:",error);set({authUser:null});}
+  finally{set({isCheckingAuth:false});}
+ }
+}),{name:"auth-storage",partialize:(state)=>({authUser:state.authUser})}));
+
+export default useAuthStore;
